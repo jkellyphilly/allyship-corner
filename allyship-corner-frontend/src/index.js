b389cf6d7605f57f92f2c35cc1e38ec54cb95347
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-02 12:41:21 -0400

    ability to create user from sign up

--- allyship-corner-frontend/src/index.js
+++ allyship-corner-frontend/src/index.js
@@ -1,17 +1,100 @@
 const BASE_URL = "http://localhost:3000";
 const EVENTS_URL = `${BASE_URL}/events`;
 const COMMENTS_URL = `${BASE_URL}/comments`;
+const USERS_URL = `${BASE_URL}/users`;
 let main = document.getElementsByTagName('main')[0];
 let addEvent = false;
+let currentUsername;
+let currentUserId;
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Build the hidden form where users can create a new event
-  showAddEvent();
+  // Show sign up option
+  makeSignUpAndLogin();
 
-  // Fetch and render our information
-  getEvents();
+  // // Build the hidden form where users can create a new event
+  // showAddEvent();
+  //
+  // // Fetch and render our information
+  // getEvents();
 })
+
+function makeSignUpAndLogin() {
+  let welcomeUsers = document.createElement('div');
+  let newUserSection = document.createElement('div');
+  let loginSection = document.createElement('div');
+
+  let newUser = document.createElement('form');
+
+  let username = document.createElement('li');
+  let usernameLabel = document.createElement('label');
+  usernameLabel.innerHTML = "Username: ";
+  let usernameInput = document.createElement('input');
+  usernameInput.setAttribute("type", "text");
+
+  username.appendChild(usernameLabel);
+  username.appendChild(usernameInput);
+
+  let password = document.createElement('li');
+
+  let passwordLabel = document.createElement('label');
+  passwordLabel.innerHTML = "Password: ";
+
+  let passwordInput = document.createElement('input');
+  passwordInput.setAttribute("type", "password");
+
+  password.appendChild(passwordLabel);
+  password.appendChild(passwordInput);
+
+  let submitUser = document.createElement('button');
+  submitUser.innerHTML = ">";
+  submitUser.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(usernameInput.value, passwordInput.value);
+    createNewUser(usernameInput.value, passwordInput.value);
+  })
+
+  newUser.appendChild(username);
+  newUser.appendChild(password);
+  newUser.appendChild(submitUser);
+  newUserSection.appendChild(newUser);
+
+  welcomeUsers.appendChild(newUserSection);
+
+  main.appendChild(welcomeUsers);
+}
+
+function createNewUser(username, password) {
+
+  let formData = {
+    user: {
+      username: username,
+      password: password
+    }
+  }
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  console.log(configObj);
+
+  fetch(USERS_URL, configObj)
+  .then(resp => resp.json())
+  .then(resp => console.log(resp))
+  .catch(err => console.log(err))
+
+  //   {
+  //   const newEvent = new Event(event.data.id, event.data.attributes);
+  //   newEvent.renderEventCard();
+  // })
+
+}
 
 function showAddEvent() {
   // Show/hide the new event form
